720d900308aded3439f3fbf1c058b9fb4a67abb4
james <jameskuhr@gmail.com>
2017-01-16 18:55:13 -0600

    First React App for ACA

---
+++ my-frst-react-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const App = ()=>{
+    return(
+    <h1>Hello React!</h1>
+    );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
